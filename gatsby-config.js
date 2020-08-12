96ce6a4cf129f453082fd416e8e14849182ad75b
Sean C Davis <scdavis41@gmail.com>
2020-08-12 16:21:58 -0400

    Hook up sanity

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,20 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: `production`,
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.SANITY_TOKEN,
+
+        // If the Sanity GraphQL API was deployed using `--tag <name>`,
+        // use `graphqlTag` to specify the tag name. Defaults to `default`.
+        graphqlTag: "default",
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,36 +2,33 @@ const fs = require("fs")
 const path = require("path")
 
 exports.onPostBuild = async ({ graphql }) => {
-  // await graphql(`
-  //   {
-  //     posts: allMarkdownRemark {
-  //       edges {
-  //         node {
-  //           frontmatter {
-  //             title
-  //             date(formatString: "MM-DD-YYYY")
-  //           }
-  //           html
-  //           fileAbsolutePath
-  //         }
-  //       }
-  //     }
-  //   }
-  // `).then(result => {
-  //   const postsPath = "./public/posts"
-  //   const posts = result.data.posts.edges.map(({ node }) => node)
-  //   if (!fs.existsSync(postsPath)) fs.mkdirSync(postsPath)
-  //   posts.map(post => {
-  //     const slug = path.basename(
-  //       post.fileAbsolutePath,
-  //       path.extname(post.fileAbsolutePath)
-  //     )
-  //     const data = {
-  //       ...post.frontmatter,
-  //       slug: slug,
-  //       body: post.html,
-  //     }
-  //     fs.writeFileSync(`${postsPath}/${slug}.json`, JSON.stringify(data))
-  //   })
-  // })
+  await graphql(`
+    {
+      posts: allSanityPost {
+        edges {
+          node {
+            title
+            date
+            slug {
+              current
+            }
+            body
+          }
+        }
+      }
+    }
+  `).then(result => {
+    const postsPath = "./public/posts"
+    const posts = result.data.posts.edges.map(({ node }) => node)
+
+    if (!fs.existsSync(postsPath)) fs.mkdirSync(postsPath)
+
+    posts.map(post => {
+      const data = {
+        ...post,
+        slug: post.slug.current,
+      }
+      fs.writeFileSync(`${postsPath}/${data.slug}.json`, JSON.stringify(data))
+    })
+  })
 }
